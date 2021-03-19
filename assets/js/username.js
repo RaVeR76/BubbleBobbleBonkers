88787efd3d75ccd9bac788ad044be772e52fdd14
RaVeR76 <leefoster76@live.co.uk>
2021-03-19 00:49:36 +0000

    Add js to get username & display short welcome message to them for test purps

--- assets/js/username.js
+++ assets/js/username.js
@@ -3,13 +3,18 @@ function handleSubmit(event) {
     event.preventDefault();
 
     let user = document.getElementById('username');
-   
     console.log('Username:', user.value);
+
+    if (user.value === "Roisin") {
+        let welcome = document.getElementById("welcome");
+        welcome.innerHTML = `<h3>Welcome To My Game ${ user.value } You SEXY Wee BEAR !!!</h3>`
+    } else {
+        let welcome = document.getElementById("welcome");
+        welcome.innerHTML = `<h3>Fuck away off from my game ${ user.value } you CUNT !!!</h3>`
+    }
+
 }
 
 // This gets the form and attaches an event listener to it
 let form = document.getElementById('uname');
 form.addEventListener('submit', handleSubmit);
-
-let welcome = document.getElementById("welcome");
-welcome.innerHTML = "<h3>Welcome To My Game ${user.value}</h3>"
