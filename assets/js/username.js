60fcdd14abff296578fe8ee5f0a222df8f4cef09
RaVeR76 <leefoster76@live.co.uk>
2021-03-26 18:53:32 +0000

    Test better chat code ... simpler

--- assets/js/username.js
+++ assets/js/username.js
@@ -1,5 +1,9 @@
 let user = document.getElementById('username');
-    console.log('Username:', user.value);
+
+const bubCharacterRef = document.querySelector('bub-talk');
+const bobCharacterRef = document.querySelector('bob-talk');
+console.log(bubCharacterRef);
+console.log(bobCharacterRef);
 
 
 function handleSubmit(event) {
@@ -14,18 +18,15 @@ function handleSubmit(event) {
     localStorage.setItem("GameName", gamename);   // Used this method for transferring the users Game Name from Intro Zone to The Game Zone so I can interact with them during the game :)
 
 
+    let bubposition = document.getElementById("bub-position");
+    bubposition.innerHTML = `<img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">`
+
     let bubtalk = document.getElementById("bub-talk");
     bubtalk.innerHTML = `
     <div class="speech-bubs">
         <p>Hey <b>${ user.value }</b>, pleased to meet you ... my name is <b>Bub</b> !</p> 
         <p>Click Me !</p>
     </div>`
-
-    let bubposition = document.getElementById("bub-position");
-    bubposition.innerHTML = `
-    <a href="javascript:void(0);" onclick="bubTalk();">
-        <img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">
-    </a>`
 
     //document.getElementById('username').value = ''; // Removes the username from the input field once button is hit
 
@@ -39,57 +40,74 @@ form.addEventListener('submit', handleSubmit);
 
 
 
-function bubTalk() {
+//function bubTalk() {
     // Prevent the default submit action (more on this in a couple units)
-    event.preventDefault();
+//    event.preventDefault();
 
-    let bubtalk = document.getElementById("bub-talk");
-    bubtalk.innerHTML = `
+//    let bubtalk = document.getElementById("bub-talk");
+//    bubtalk.innerHTML = `
+//    <div class="speech-bubs">
+//        <p>Just so you know <b>${ user.value }</b>, I am & always will be <b>Player One</b> !</p>
+//    </div>`
+
+//    let bubposition = document.getElementById("bub-position");
+//    bubposition.innerHTML = `
+//    <a href="javascript:void(0);" onclick="bobTalk();">
+//        <img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">
+ //   </a>`
+//}
+
+
+
+//function bobTalk() {
+//    // Prevent the default submit action (more on this in a couple units)
+//    event.preventDefault();
+
+//    let bobtalk = document.getElementById("bob-talk");
+//    bobtalk.innerHTML = `
+//    <div class="speech-bubs2">
+//        <p>Hey <b>${ user.value }</b>, don't listen to him. I've heard this crap for 35 years !!!</p>
+//    </div>`
+
+
+//    let bobposition = document.getElementById("bob-position");
+//    bobposition.innerHTML = `
+//    <a href="javascript:void(0);" onclick="bubTalk2();">
+//        <img src="assets/cards/bob-card.jpg" alt="Wee Bubble Bobble Bub" class="bob-size">
+//    </a>`
+//}
+
+
+//function bubTalk2() {
+    // Prevent the default submit action (more on this in a couple units)
+ //   event.preventDefault();
+
+ //   let bubtalk = document.getElementById("bub-talk");
+//    bubtalk.innerHTML = `
+//    <div class="speech-bubs">
+//        <p><b>${ user.value }</b>, that's <b>Bob</b> by the way ... the <b>"second"</b> best player of the game !</p>
+  //  </div>`
+
+  //  let bubposition = document.getElementById("bub-position");
+  //  bubposition.innerHTML = `
+ //   <a href="javascript:void(0);" onclick="bobTalk();">
+ //       <img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">
+ //   </a>`
+//}
+
+function characterTalk(characterColor, event) {
+  event.preventDefault();
+  if (characterColor === 'green') {
+    bubCharacterRef.innerHTML = `
+      <div class="speech-bubs-bub">
+          <p>Hey <b>${user.value}</b>, don't listen to him. I've heard this crap for 35 years !!!</p>
+      </div>`;
+  } else {
+    bobCharacterRef.innerHTML = `
     <div class="speech-bubs">
-        <p>Just so you know <b>${ user.value }</b>, I am & always will be <b>Player One</b> !</p>
-    </div>`
-
-    let bubposition = document.getElementById("bub-position");
-    bubposition.innerHTML = `
-    <a href="javascript:void(0);" onclick="bobTalk();">
-        <img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">
-    </a>`
+        <p><b>${user.value}</b>, that's <b>Bob</b> by the way ... the <b>"second"</b> best player of the game !</p>
+    </div>`;
+  }
 }
-
-
-
-function bobTalk() {
-    // Prevent the default submit action (more on this in a couple units)
-    event.preventDefault();
-
-    let bobtalk = document.getElementById("bob-talk");
-    bobtalk.innerHTML = `
-    <div class="speech-bubs2">
-        <p>Hey <b>${ user.value }</b>, don't listen to him. I've heard this crap for 35 years !!!</p>
-    </div>`
-
-
-    let bobposition = document.getElementById("bob-position");
-    bobposition.innerHTML = `
-    <a href="javascript:void(0);" onclick="bubTalk2();">
-        <img src="assets/cards/bob-card.jpg" alt="Wee Bubble Bobble Bub" class="bob-size">
-    </a>`
-}
-
-
-function bubTalk2() {
-    // Prevent the default submit action (more on this in a couple units)
-    event.preventDefault();
-
-    let bubtalk = document.getElementById("bub-talk");
-    bubtalk.innerHTML = `
-    <div class="speech-bubs">
-        <p><b>${ user.value }</b>, that's <b>Bob</b> by the way ... the <b>"second"</b> best player of the game !</p>
-    </div>`
-
-    let bubposition = document.getElementById("bub-position");
-    bubposition.innerHTML = `
-    <a href="javascript:void(0);" onclick="bobTalk();">
-        <img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">
-    </a>`
-}
+bubCharacterRef.addEventListener('click', characterTalk("green"))
+bobCharacterRef.addEventListener('click', characterTalk("blue"))
