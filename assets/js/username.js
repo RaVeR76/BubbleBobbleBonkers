979ff58ca0236200a6a7009b08239ba7c8144d41
RaVeR76 <leefoster76@live.co.uk>
2021-04-10 18:48:23 +0000

    Amend Intro chat to make it work better with less code

--- assets/js/username.js
+++ assets/js/username.js
@@ -6,8 +6,8 @@ const bubChat = document.getElementById("bub-talk");
 const bobChat = document.getElementById("bob-talk");
 const bubCharacterRef = document.getElementById("bub-position");
 const bobCharacterRef = document.getElementById("bob-position");
-const speechBubs = document.getElementsByClassName("bub-speech");
-const speechBobs = document.getElementsByClassName("bob-speech");
+//const speechBubs = document.getElementsByClassName("bub-speech");
+//const speechBobs = document.getElementsByClassName("bob-speech");
 
 
 
@@ -16,18 +16,18 @@ function handleSubmit(event) {
     
     event.preventDefault();
 
-    document.getElementById("bob-talk").innerHTML = "";     // Added these two lines so it will remove Bob's image & his speech bubble .... if the user enters a new username
-    document.getElementById("bob-position").innerHTML = "";
+    bubChat.innerHTML = "";     // Added these two lines so it will remove Bob's image & his speech bubble .... if the user enters a new username
+    bobChat.innerHTML = "";
 
 
     let gamename = user.value;
     localStorage.setItem("GameName", gamename);   // Used this method for transferring the users Game Name from Intro Zone to The Game Zone so I can interact with them during the game :)
 
 
-    //let bubposition = document.getElementById("bub-position");
+    
     bubCharacterRef.innerHTML = `<img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">`
 
-   // let bubtalk = document.getElementById("bub-talk");
+  
    bubChat.innerHTML = `
     <div class="bub-speech">
         <p>Hey <b>${ user.value }</b>, pleased to meet you ... my name is <b>Bub</b> !</p> 
@@ -35,25 +35,17 @@ function handleSubmit(event) {
     </div>`
 
     setTimeout(function(){
-       // let bobposition = document.getElementById("bob-position");
+       
        bobCharacterRef.innerHTML = `<img src="assets/cards/bob-card.jpg" alt="Wee Bubble Bobble Bob" class="bob-size">`
 
-       // let bobtalk = document.getElementById("bob-talk");
+     
         bobChat.innerHTML = `
         <div class="bob-speech">
             <p>And I am <b>Bob</b>, pleased to meet you ... <b>${ user.value }</b> !</p> 
         </div>`    
     },2000);
 
-
-
-
-
-
-
-
     //document.getElementById('username').value = ''; // Removes the username from the input field once button is hit
-    
 
 }
 
@@ -74,37 +66,20 @@ function bubTalk(event) {
         <p>Just so you know <b>${ user.value }</b>, I am & always will be <b>Player One</b> !</p>
     </div>`
 
-  
-
-
 }
 
 
-  // let bubposition = document.getElementById("bub-position");
-   //bubCharacterRef.innerHTML = `
-   // <a href="javascript:void(0);" onclick="bobTalk();">
-   //     <img src="assets/cards/bub-card.jpg" alt="Wee Bubble Bobble Bub" class="bub-size">
-   // </a>`
+function bobTalk(event) {
+   //  Prevent the default submit action (more on this in a couple units)
+    event.preventDefault();
 
 
+    bobChat.innerHTML = `
+    <div class="bob-speech">
+    <p>Hey <b>${ user.value }</b>, don't listen to him. I've heard this crap for 35 years !!!</p>
+    </div>`
 
-
-
-
-
-//function bobTalk(event) {
-    // Prevent the default submit action (more on this in a couple units)
- //   event.preventDefault();
-
-//    let bobtalk = document.getElementById("bob-talk");
-//    bobtalk.innerHTML = `
-//    <div class="speech-bubs2">/        <p>Hey <b>${ user.value }</b>, don't listen to him. I've heard this crap for 35 years !!!</p>
-//    </div>`
-
-
-//    let bobposition = document.getElementById("bob-position");
-//    bobposition.innerHTML = `<img src="assets/cards/bob-card.jpg" alt="Wee Bubble Bobble Bub" class="bob-size">`
-//}
+}
 
 
 //function bubTalk2(event) {
@@ -127,7 +102,7 @@ function bubTalk(event) {
 
 
 bubCharacterRef.addEventListener("click", bubTalk, false);
-//bubCharacterRef.addEventListener("click", bubTalk2, false);
+bobCharacterRef.addEventListener("click", bobTalk, false);
 
 
 
